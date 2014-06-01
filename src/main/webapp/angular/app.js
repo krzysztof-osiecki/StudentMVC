ab923f92308efd6a1668ab0154cb31d6d594cd4a
michal-stankiewicz <michal.j.stankiewicz@gmail.com>
2014-06-01 18:56:20 +0200

    formularz z ocenami gotowy

--- src/main/webapp/angular/app.js
+++ src/main/webapp/angular/app.js
@@ -2,6 +2,7 @@ var app = angular.module('student', []);
 //TODO gdzieś na koniec ogarnac jquery z angularem
 app.controller('MainCtrl', function($scope,$http) {
   $scope.name = "Felipe";
+  
   $scope.pokazStudentow=function(){
 	$http.get('/AngularSpringApp/studenci/lista').
                 success(function(data) {
@@ -16,12 +17,31 @@ app.controller('MainCtrl', function($scope,$http) {
 	            });
 	        }
   $scope.getWykladyStudenta=function(studentId){
-	  alert('/AngularSpringApp/studenci/wykladyStudenta?studentId='+studentId);
+	  //alert('/AngularSpringApp/studenci/wykladyStudenta?studentId='+studentId);
 	  $http.get('/AngularSpringApp/studenci/wykladyStudenta?studentId='+studentId).
 	  		success(function(data){
 	  			$scope.zaliczeniaStudenta=data;
-	  			alert(data);
+
+	  			$scope.grades = [{value:"2"},
+	  			                 {value:"2.5"},
+	  			                 {value:"3"},
+	  			                 {value:"3.5"},
+	  			                 {value:"4"},
+	  			                 {value:"4.5"},
+	  			                 {value:"5"}];
+	  			$scope.grade = [];
+	  			for(var i = 0; i < data.length ; i++){
+	  				for(var j = 0; j < $scope.grades.length; j++){
+	  					
+	  					if(data[i].ocena == $scope.grades[j].value){
+	  						$scope.grade[i] = $scope.grades[j];
+	  						break;
+	  					}
+	  				}
+	  			}
+	  			//$scope.grade[0] = $scope.grades[2];
 	  			console.log(data);
+	  			$("#studentIdFullData_"+studentId).show();
 	  		});
   };
 });
--- src/main/webapp/angular/js/view.js
+++ src/main/webapp/angular/js/view.js
@@ -2,6 +2,6 @@
 $(document).ready(function(){
 	$("body").on("click","button.showDetails",function(){
 		$(".studentDetails").not("#studentIdFullData_"+$(this).attr("id").split("_")[1]).hide();
-		$("#studentIdFullData_"+$(this).attr("id").split("_")[1]).toggle();
+		//$("#studentIdFullData_"+$(this).attr("id").split("_")[1]).toggle();
 	});
 });
